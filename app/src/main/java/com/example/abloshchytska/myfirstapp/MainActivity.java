c013ef97fc12c4475d172399ee29483d303c7dde
bloshchytska <anastasiia.bloshchytska@comsysto.com>
2018-08-11 23:43:52 +0200

    tryed another version of capturing (withh camerapreview), added new class, changed layout

--- app/src/main/java/com/example/abloshchytska/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/abloshchytska/myfirstapp/MainActivity.java
@@ -1,33 +1,35 @@
 package com.example.abloshchytska.myfirstapp;
 
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Typeface;
+import android.hardware.Camera;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v4.content.ContextCompat;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.FrameLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+import static android.content.ContentValues.TAG;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+
+public class MainActivity extends AppCompatActivity  {
     public static Bitmap croppedImage;
     public static Uri imageCaptureUri;
 
     public static final int REQUEST_IMAGE_CROP = 2;
     private final int REQUEST_IMAGE_CAPTURE = 1;
 
+    private Camera mCamera;
+    private CameraPreview mPreview;
 
 
     // Used to load the 'native-lib' library on application startup.
@@ -37,27 +39,99 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
+    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
 
 
-
-        Button btnCamera = findViewById(R.id.btnCamera);
-
-        btnCamera.setOnClickListener(new View.OnClickListener() {
+        Button btnCaptureImage = findViewById(R.id.button_capture);
+        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            public void onClick(View v) {
+                // Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+               // startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                mCamera.takePicture(null, null, mPicture);
             }
         });
 
     }
 
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions");
+                return;
+            } try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+                Log.d(TAG, "Write");
+
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+
+
+    /** Create a File for saving an image  */
+    private static File getOutputMediaFile(int type){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "ScandiToKnow");
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("ScandiToKnow", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+            Log.d("ScandiToKnow", "saved");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    /*
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -66,51 +140,14 @@ public class MainActivity extends AppCompatActivity {
 
             if (requestCode == REQUEST_IMAGE_CAPTURE) {
                 imageCaptureUri = data.getData();
-                performImageCrop(imageCaptureUri);
-
-            } else if(requestCode == REQUEST_IMAGE_CROP){
-                Bundle extras = data.getExtras();
-                croppedImage = extras.getParcelable("data");
-                showTestView();
+                //performImageCrop(imageCaptureUri);
             }
 
         } else if (resultCode == Activity.RESULT_CANCELED) {
             // TODO: error messange
         }
     }
+    */
 
 
-
-    public void performImageCrop(Uri picUri){
-        try {
-            Intent cropIntent = new Intent("com.android.camera.action.CROP");
-            cropIntent.setDataAndType(picUri, "image/*");
-            cropIntent.putExtra("crop", "true");
-            //indicate aspect of desired crop
-            cropIntent.putExtra("aspectX", 1);
-            cropIntent.putExtra("aspectY", 1);
-            cropIntent.putExtra("outputX", 256);
-            cropIntent.putExtra("outputY", 256);
-            cropIntent.putExtra("scale", true);
-            cropIntent.putExtra("return-data", true);
-            startActivityForResult(cropIntent, REQUEST_IMAGE_CROP);
-
-        } catch(ActivityNotFoundException anfe){
-            String errorMessage = "Whoops - your device doesn't support the crop action!";
-            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
-            toast.show();
-        }
-    }
-
-
-
-    public void showTestView() {
-        Intent intent = new Intent(this, TestHashingImages.class);
-        startActivity(intent);
-    }
-
-    public void showResultMessage() {
-        Intent intent = new Intent(this, DisplayResultMessageActivity.class);
-        startActivity(intent);
-    }
 }
