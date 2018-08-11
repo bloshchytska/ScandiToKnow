8e708019959136a14734cb4762dee2e164f8437a
bloshchytska <anastasiia.bloshchytska@comsysto.com>
2018-08-11 12:47:53 +0200

    added draf calasses for camera, added new font

--- app/src/main/java/com/example/abloshchytska/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/abloshchytska/myfirstapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
